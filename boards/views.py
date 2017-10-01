43c0e94b1129c04e3ad9460733df7280de5de8c9
Erick <erickmwazonga@gmail.com>
2017-10-01 16:42:57 +0300

    create a home page with boards list

--- boards/views.py
+++ boards/views.py
@@ -7,13 +7,6 @@ from django.http import HttpResponse
 from .models import Board
 
 # Create your views here.
-def home(response):
+def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards':boards})
