6295a93274377cb17df86493d19b0f6fc88cc4c5
Erick <erickmwazonga@gmail.com>
2017-10-01 17:27:51 +0300

    create test for board_topics

--- boards/tests.py
+++ boards/tests.py
@@ -4,7 +4,8 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
 
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 # Create your tests here.
 class HomeTests(TestCase):
@@ -18,4 +19,20 @@ class HomeTests(TestCase):
         self.assertEquals(view.func, home)
 
 
-        
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='YII2', description='Onfon base Framework.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk':1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk':99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolve_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
