8d0a6952d181c101d6f265f17649ef96ed00711c
Erick <erickmwazonga@gmail.com>
2017-10-01 18:26:44 +0300

    implement template inheritance

--- boards/tests.py
+++ boards/tests.py
@@ -12,10 +12,10 @@ class HomeTests(TestCase):
     def setUp(self):
         self.board = Board.objects.create(name='Django', description='Django board.')
         url = reverse('home')
-        response = self.client.get(url)
+        self.response = self.client.get(url)
 
     def test_home_view_status_code(self):
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolve_home_view(self):
         view = resolve('/')
@@ -43,3 +43,9 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolve_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
