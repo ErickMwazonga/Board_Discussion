c12c4609a62e6ed542e7d968b1b958094c440003
Erick <erickmwazonga@gmail.com>
2017-10-01 16:33:28 +0300

    create a list of boards using fbv

--- boards/views.py
+++ boards/views.py
@@ -4,6 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Board
+
 # Create your views here.
 def home(response):
-    return HttpResponse('I LOVE YOU')
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
